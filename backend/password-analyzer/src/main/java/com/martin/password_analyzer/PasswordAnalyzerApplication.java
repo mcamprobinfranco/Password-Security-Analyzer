de3d67ec4a11f1ee164154b414e9ab467c810f5e
Martin <mcamprobinfranco@gmail.com>
2026-08-08 19:30:35 +0200

    Esqueleto inicial con Spring Boot

---
+++ backend/password-analyzer/src/main/java/com/martin/password_analyzer/PasswordAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.martin.password_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordAnalyzerApplication.class, args);
+	}
+
+}
---
+++ backend/password-analyzer/src/test/java/com/martin/password_analyzer/PasswordAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.password_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
